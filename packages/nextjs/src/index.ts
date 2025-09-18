77453e2a49ff89d2be289e27f5b7010e9f8230a0
doong-jo <sdong001@gmail.com>
2025-09-18 13:34:03 +0900

    chore: remove test comments from nextjs and react package files

--- packages/nextjs/src/index.ts
+++ packages/nextjs/src/index.ts
@@ -1,9 +1,8 @@
 // Next.js Image Loader for Snapkit
-// Test: nextjs package change detection v2
 export { createSnapkitLoader, snapkitLoader } from './image-loader';
 
 // Re-export types needed for Next.js integration
 export type {
-  ImageLoader, ImageLoaderParams, SnapkitLoaderOptions,
+  ImageLoader, ImageLoaderParams, SnapkitLoaderOptions
 } from '@snapkit-studio/core';
 
